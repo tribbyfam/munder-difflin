85712813b74d1671fe73047dbd24596c9947244a
Mac <mac@MacBook-Pro.fios-router.home>
2018-08-08 22:07:46 -0500

    final. onclick still doesn't work

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 
-let moveBtn = document.getElementById(custom) 
-let leftOffset = 0;
-let topOffset = 800;
 
-moveBtn.addEventListener("onload", moveCustom)
+// Tried to make price popup when you click on the picture, couldn't do it, keeps telling me displayPrice is not defined.
+
+let myImage = document.getElementsByTagName("flip")[0]
+let price = document.getElementsByTagName("price")[0]
+
+myImage.addEventListener("onclick", displayPrice)
+
+function displayPrice(event){
+    if (i = 0; i < myImage.length; i++;) {
+        event.target.style.display = "block"
+    }
+}
