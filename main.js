2c691ad7bd837665d3cc992ad871976bed6b170d
Mac <mac@MacBook-Pro.fios-router.home>
2018-08-15 19:37:00 -0500

    made changes to js code, still not fully working

--- main.js
+++ main.js
@@ -1,14 +1,28 @@
 
 
 // Tried to make price popup when you click on the picture, couldn't do it, keeps telling me displayPrice is not defined.
+let myImages = document.getElementsByClassName("flip")
 
-let myImage = document.getElementsByTagName("flip")[0]
-let price = document.getElementsByTagName("price")[0]
-
-myImage.addEventListener("onclick", displayPrice)
-
-function displayPrice(event){
-    if (i = 0; i < myImage.length; i++;) {
-        event.target.style.display = "block"
+let prices = document.getElementsByClassName("price")
+for (let i=0; i<myImages.length; i++){
+    myImage.addEventListener("click", displayPrice)
+    function displayPrice(){
+    
+        price.style.display="block"
     }
+    
 }
+
+
+
+
+
+
+
+
+
+
+
+
+    // for (let i = 0; i < myImage.length; i++) {
+    //     event.target.style.display = "block"
