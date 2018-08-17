c6d0561cd6547df6461ce338bf0e60d4cea996a1
Mac <mac@MacBook-Pro.local>
2018-08-17 06:55:54 -0500

    made first image change, working on all images to change onclick

--- main.js
+++ main.js
@@ -1,18 +1,55 @@
 
 
 // Tried to make price popup when you click on the picture, couldn't do it, keeps telling me displayPrice is not defined.
-let myImages = document.getElementsByClassName("flip")
+// let myImages = document.getElementsByClassName("flip")
 
-let prices = document.getElementsByClassName("price")
-for (let i=0; i<myImages.length; i++){
-    myImage.addEventListener("click", displayPrice)
-    function displayPrice(){
+// let prices = document.getElementsByClassName("price")
+// for (let i=0; i<myImages.length; i++){
+//     myImage.addEventListener("click", displayPrice)
+//     function displayPrice(){
     
-        price.style.display="block"
-    }
+//         price.style.display="block"
+//     }
     
+// }
+
+let tracker1 = 'image1';
+function change(){
+    let image = document.getElementById('flip1');
+
+    if(tracker1 == 'image1'){
+        image.src="./images/off-folded-menue.jpeg"
+        tracker1 = 'image2'
+    } else {
+        image.src="./images/off-roll-paper.jpeg"
+        tracker1 = 'image1'
+    } 
 }
 
+let tracker2 = 'image1'
+function change(){
+    let image = document.getElementById('flip2');
+
+    if(tracker2 == 'image1'){
+        image.src="images/off-sch-binder.jpeg"
+        tracker2 = 'image2'
+    } else {
+        image.src="./images/off-sch-notepads.jpg"
+        tracker2 = 'image1'
+    }
+
+//     function change(){
+//         let image = document.getElementById('flip3');
+//         let tracker = 'image1';
+
+//     if(tracker == 'image1'){
+//         image.src="images/off-sch-binder.jpeg"
+//         tracker = 'image2'
+//     } else {
+//         image.src="./images/off-sch-notepads.jpg"
+//         tracker = 'image1'
+//     }
+// }
 
 
 
