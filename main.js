c0713e138f48a77f19d588dbde199d917209204c
Mac <mac@MacBook-Pro.local>
2018-08-17 09:19:28 -0500

    fixed all images to change on time and stop on click

--- main.js
+++ main.js
@@ -13,44 +13,57 @@
     
 // }
 
+// let large = new large[2];
+// let office = new office[2];
+// let colors = new colors[2];
+// for ( i=0; i<large.length; i++){
+//     large[i] = loadImage("large" + i + ".jpg")
+// }
+
 let tracker1 = 'image1';
 function change1(){
     let image = document.getElementById('flip1');
 
     if(tracker1 == 'image1'){
-        image.src="./images/off-folded-menue.jpeg"
+        image.src="./images/large2.jpg"
         tracker1 = 'image2'
     } else {
-        image.src="./images/off-roll-paper.jpeg"
+        image.src="./images/large1.jpg"
         tracker1 = 'image1'
     } 
 }
-
 let timer = setInterval('change1()', 2000);
 
-// let tracker2 = 'image1'
-// function change2(){
-//     let image = document.getElementById('flip2');
 
-//     if(tracker2 == 'image1'){
-//         image.src="images/off-sch-binder.jpeg"
-//         tracker2 = 'image2'
-//     } else {
-//         image.src="./images/off-sch-notepads.jpg"
-//         tracker2 = 'image1'
-//     }
+let tracker2 = 'image1'
+function change2(){
+    let image = document.getElementById('flip2');
 
-//     function change2(){
-//         let image = document.getElementById('flip3');
+    if(tracker2 == 'image1'){
+        image.src="./images/office2.jpg"
+        tracker2 = 'image2'
+    } else {
+        image.src="./images/office1.jpg"
+        tracker2 = 'image1'
+    }
+}   
+let timer2 = setInterval('change2()', 2100);
 
-//     if(tracker == 'image1'){
-//         image.src="images/off-sch-binder.jpeg"
-//         tracker = 'image2'
-//     } else {
-//         image.src="./images/off-sch-notepads.jpg"
-//         tracker = 'image1'
-//     }
-// }
+
+let tracker3 = 'image1'
+function change3(){
+    let image = document.getElementById('flip3');
+
+    if(tracker3 == 'image1'){
+        image.src="./images/color2.jpg"
+        tracker3 = 'image2'
+    } else {
+        image.src="./images/color1.jpg"
+        tracker3 = 'image1'
+    }
+}
+let timer3 = setInterval('change3()', 2200);
+
 
 
 
